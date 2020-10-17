dd002c1b595ed56869e8cff0970a80e758e2c796
dinpwr <58038112+dinpwr@users.noreply.github.com>
2020-10-17 12:38:28 +0300

    switched from embedded db to docker image

---
+++ src/main/java/din/springframework/sfgjms/listener/HelloMessageListener.java
@@ -0,0 +1,44 @@
+package din.springframework.sfgjms.listener;
+
+import din.springframework.sfgjms.config.JmsConfig;
+import din.springframework.sfgjms.model.HelloWorldMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.handler.annotation.Headers;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+//@Component
+public class HelloMessageListener {
+
+    private final JmsTemplate jmsTemplate;
+
+    @JmsListener(destination = JmsConfig.MY_QUEUE)
+    public void listen(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers,
+                       Message message) {
+        //System.out.println("I got a message!");
+
+        //System.out.println(helloWorldMessage);
+    }
+
+
+    @JmsListener(destination = JmsConfig.MY_SEND_RCV_QUEUE)
+    public void listenForHello(@Payload HelloWorldMessage helloWorldMessage, @Headers MessageHeaders headers,
+                       Message message) throws JMSException {
+
+        HelloWorldMessage helloMessage = HelloWorldMessage
+                .builder()
+                .id(UUID.randomUUID())
+                .message("World!")
+                .build();
+
+        jmsTemplate.convertAndSend(message.getJMSReplyTo(), helloMessage);
+    }
+}
